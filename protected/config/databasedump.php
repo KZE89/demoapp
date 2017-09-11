d077f57427881be977b21e3ba7f599c07ea4e3fe
Azat Kabirov <azat.kabirov89@yandex.ru>
2017-09-11 14:52:38 +0300

    Composer.json

---
+++ protected/config/databasedump.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host={host};dbname={dbname}',
+	'emulatePrepare' => true,
+	'username' => '',
+	'password' => '',
+	'charset' => 'utf8',
+	
+);
